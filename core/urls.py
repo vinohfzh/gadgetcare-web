19d09522cde11fd28d37a26e906721b332552d72
vinohfzh <email@example.com>
2026-05-31 04:06:24 +0700

    Add Page Layanan & Process/Fix Page Cek Status

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import home
+from .views import home, layanan
 
 urlpatterns = [
     path('', home, name='home'),
+    path('layanan/', layanan, name='layanan'),
 ]
